5c414e116fad9ec9155776e08a97447f3f0bf12d
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-20 17:29:54 +0200

    refactor: update variable assignments in index.php

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
-$_1a = 4;
-$b = 5;
-$c = $a + $b;
-echo $c;
+// $nome1 è una variabile che contiene il nome "Gianluca".
+$nome1 = 'Gianluca';
+
+// $nome2 e $nome3 puntano alla stessa memoria di $nome1.
+// Questo significa che se $nome2 viene modificato, $nome1 e $nome3 verranno modificati in modo equivalente.
+$nome2 = &$nome1;
+$nome3 = &$nome2;
+
+// Modifico $nome2, che da questo senso anche $nome1 e $nome3.
+$nome2 = 'Bellissimo';
+
+// Modifico $nome1, che da questo senso anche $nome2 e $nome3.
+$nome1 = 'suka';
+
+// Stampa il valore di $nome3, che è uguale a $nome1, che è uguale a $nome2.
+echo $nome3;
