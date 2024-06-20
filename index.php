fa5885e6d4f6523184e8b12fe67d3c5de5747f32
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-20 17:53:06 +0200

    refactor: update index.php with constants and remove redundant variables

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
-// $nome1 è una variabile che contiene il nome "Gianluca".
-$nome1 = 'Gianluca';
 
-// $nome2 e $nome3 puntano alla stessa memoria di $nome1.
-// Questo significa che se $nome2 viene modificato, $nome1 e $nome3 verranno modificati in modo equivalente.
-$nome2 = &$nome1;
-$nome3 = &$nome2;
+// Le costanti
+define('APP_SUKA', 1);
 
-// Modifico $nome2, che da questo senso anche $nome1 e $nome3.
-$nome2 = 'Bellissimo';
+// Oppure scrivere
+const APP_VERSION = 1;
 
-// Modifico $nome1, che da questo senso anche $nome2 e $nome3.
-$nome1 = 'suka';
+echo APP_VERSION;
 
-// Stampa il valore di $nome3, che è uguale a $nome1, che è uguale a $nome2.
-echo $nome3;
+/* -------------------------------------------------------------------------- */
+
+$user = 'USER_STANDARD';
+
+define('STATUS_'.$user, 999999);
+
+echo STATUS_USER_STANDARD . "\n";
