db8a85947b181de3ee8ced394f2a8a4c28de816d
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-21 12:32:40 +0200

    Add control structure example with if-else statement in index.php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 
-// Assegna il valore 2.5 alla variabile $a
-$a = 2.5;
+// Strutture di controllo (if)
 
-// Verifica se la variabile $a è stata definita (in questo caso, sempre verrà restituito true)
-$test = isset($a);
+$meteoDomani = 'sole';
 
-// Restituisce il tipo di dato della variabile $a
-$test = gettype($a);
-
-// Stampa a schermo il tipo di dato della variabile $a
-var_dump(gettype($a));
-
+if ($meteoDomani == 'sole') {
+  echo 'Esco a fare una passeggiata';
+} else {
+  echo 'Resto a casa';
+}
