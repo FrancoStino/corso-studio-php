4ca1e791011cc14e4a324b636cb2e2a64732e274
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-21 10:32:40 +0200

    Refactor index.php: Replace data type examples with a new function and its usage

--- index.php
+++ index.php
@@ -1,48 +1,10 @@
 <?php
 
-// Tipi di dati
+function funzione1($valore)
+{
+  return $valore + 1.5;
+} 
 
-// Stringa
+echo funzione1(0);
 
-$nome = "Marco";
-
-
-// Intero
-
-$eta = 30;
-
-
-// Float
-
-$peso = 80.5;
-
-
-// Boolean
-
-$vero = true; // Restituisce 1
-$falso = false; // Restituisce 0
-
-
-// Array
-
-$nomi = array("Marco", "Mario", "Luca");
-
-
-// Oggetto
-
-$persona = new stdClass();
-$persona->nome = "Marco";
-$persona->eta = 30;
-$persona->peso = 80.7;
-
-
-// Null
-
-$vuoto = null; // Restituisce 0
-
-
-// Stampa
-
-echo $nome . "\n";
-echo $eta . "\n";
-echo $peso . "\n";
+echo funzione1(3); 
