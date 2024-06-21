c686f54431b5d5e3fbed077cd1920b8156e978bc
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-21 14:35:07 +0200

    Add arithmetic operations example and documentation in index.php

--- index.php
+++ index.php
@@ -1,20 +1,47 @@
 <?php
 
 /**
- * Esempio di come utilizzare le funzioni is_numeric(), is_float() e is_int()
- * per verificare il tipo di dato.
+ * In questo script vengono illustrate alcune operazioni aritmetiche
+ * in PHP.
+ *
+ * @author Matteo Tempestini <matteo.tempestini@gmail.com>
+ * @version 1.0
+ * @license MIT
  */
 
-$numero = 5.3;
+// +, -, *, /, %, **
 
-// Verifica se il valore è un numero (float o int).
-$test = is_numeric($numero);
+/**
+ * Assegna al valore di $x il valore intero di '10'.
+ *
+ * @var int
+ */
+$x = (int)'10';
 
-// Verifica se il valore è un float.
-$test = is_float($numero);
+/**
+ * Visualizza il valore di $x.
+ * 
+ * @see var_dump()
+ */
+var_dump($x );
 
-// Verifica se il valore è un intero.
-$test = is_int($numero);
+/**
+ * Interrompe l'esecuzione dello script.
+ *
+ * @see exit()
+ */
+exit;
 
-// Stampa il risultato della verifica.
-var_dump($test);
+/**
+ * Assegna al valore di $y il valore intero 5.
+ *
+ * @var int
+ */
+$y = 5;
+
+/**
+ * Visualizza il risultato della somma di $x e $y.
+ *
+ * @see echo
+ */
+echo $x + $y;
