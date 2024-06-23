61ac6534996ec114a3fe98dafdd13231f59626d1
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-23 16:10:29 +0200

    Update index.php to demonstrate multidimensional arrays in PHP with examples and explanations in Italian.

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
 
 /**
- * Array unpacking example.
+ * Questo esempio mostra come creare e utilizzare array multidimensionali in PHP.
  *
- * The spread operator (...) can be used to insert the elements of an array
- * into another array. This is known as array unpacking.
+ * Un array multidimensionale è un array che contiene altri array.
+ * Ogni sottomatrice può avere una dimensione diversa dall'altra.
+ * Le matrici possono avere chiavi stringhe o numeriche.
  *
- * In this example, we have an array $a with some elements. We create a new
- * array $b by using the spread operator to insert the elements of $a into it,
- * followed by two additional elements.
+ * Nel seguente esempio, abbiamo due array multidimensionali:
  *
- * The resulting array $b will contain all the elements of $a, followed by 4 and 5.
+ * - `$multidimensionale` è un array di tre elementi, ciascuno dei quali è un array con due campi:
+ *   `ISBN` e `Titolo`.
+ *
+ * - `$multidimensionale2` è un array associativo con tre sotto-matrici.
+ *   Le sotto-matrici hanno chiavi stringhe: 'Primo', 'Secondo' e 'Terzo'.
+ *   Ognuna di queste sotto-matrici è un array associativo con due campi: `ISBN` e `Titolo`.
  */
 
-$a = [1, 'due' => 2, 3];
-$b = [...$a, 4, 5];
+$multidimensionale = [
+  ['ISBN' => 1234, 'Titolo' => 'Libro 1'],
+  ['ISBM' => 1234, 'Titolo' => 'Libro 2'],
+  ['ISBM' => 1234, 'Titolo' => 'Libro 3'],
+];
 
-var_dump($b);
+var_dump($multidimensionale);
+
+// Possiamo accedere ad un elemento multidimensionale come facciamo con un array normale.
+$item = $multidimensionale[2];
+echo $item['Titolo'];
+
+// Oppure possiamo accedere direttamente ad un elemento multidimensionale.
+echo $multidimensionale[2]['Titolo'];
+
+// Esempio di array multidimensionale associativo
+$multidimensionale2 = [
+  'Primo' => ['ISBN' => 1234, 'Titolo' => 'Libro 1'],
+  'Secondo' =>['ISBM' => 1234, 'Titolo' => 'Libro 2'],
+  'Terzo' => ['ISBM' => 1234, 'Titolo' => 'Libro 3'],
+];
+
+var_dump($multidimensionale2);
+
+// Possiamo accedere ad un elemento multidimensionale associativo come facciamo con un array normale.
+echo $multidimensionale2['Terzo']['Titolo'];
