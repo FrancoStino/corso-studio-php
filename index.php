239cdc02b467ec47e540faf62b57e26abc3968d8
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-23 13:32:26 +0200

    Aggiorna esempio per aggiungere e rimuovere elementi da un array

--- index.php
+++ index.php
@@ -1,47 +1,42 @@
 <?php
 
 /**
- * Introduzione agli array
+ * Aggiunge e rimuove elementi da un array.
  *
- * Questo esempio mostra come dichiarare e utilizzare gli array.
- * In primo luogo, si dichiara un array di nomi di alunni con la funzione count() per contare il numero di elementi.
- * Successivamente, si accede agli elementi dell'array utilizzando l'indice numerico.
- * Infine, si visualizza il contenuto dell'array con la funzione var_dump().
+ * Esempi di utilizzo:
+ *
+ * $alunni = ['Anna'];
+ *
+ * $alunni[] = 'Massimo'; // Aggiunge 'Massimo' all'ultimo elemento dell'array
+ * $alunni[] = 'Miriam'; // Aggiunge 'Miriam' all'ultimo elemento dell'array
+ *
+ * array_push($alunni, 'Giovanni', 'Maria'); // Aggiunge 'Giovanni' e 'Maria' all'ultimo elemento dell'array
+ * array_unshift($alunni, 'Antonio'); // Aggiunge 'Antonio' all'inizio dell'array
+ *
+ * array_pop($alunni); // Rimuove l'ultimo elemento dell'array
+ * array_shift($alunni); // Rimuove il primo elemento dell'array
+ *
+ * unset($alunni[1]); // Rimuove l'elemento all'indice 1 dell'array
+ * $alunni = array_values($alunni); // Riordina gli indici dell'array
+ *
+ * array_splice($alunni, 2, 1, ['Luca', 'Marco']); // Rimpiazza l'elemento all'indice 2 con 'Luca' e 'Marco'
+ *
+ * var_dump($alunni); // Stampa l'array
  */
 
-$alunni = ["Mario", "Giulia", "Gianni", "Giovanni"];
-$numeriAlunni = count($alunni);
-echo $numeriAlunni; // visualizza il numero di alunni
-echo $alunni[$numeriAlunni - 1]; // visualizza l'ultimo alunno
-echo $alunni[1]; // visualizza il secondo alunno
-var_dump($alunni); // visualizza il contenuto dell'array
+$alunni = ['Anna'];
 
+$alunni[] = 'Massimo';
+$alunni[] = 'Miriam';
 
-/**
- * Array associativi
- *
- * In questo esempio, si mostra come dichiarare e utilizzare gli array associativi.
- * Si dichiara un array associativo con chiavi come "nome", "cognome" e "eta".
- * Successivamente, si visualizza il contenuto dell'array utilizzando la notazione a parentesi quadre.
- * Infine, si usa la funzione extract() per estrarre le chiavi dell'array in variabili locali.
- */
+array_push($alunni, 'Giovanni', 'Maria');
+array_unshift($alunni, 'Antonio');
 
-$user = [
-  "nome" => "Mario",
-  "cognome" => "Rossi",
-  "eta" => 25,
-  "cii" // chiave inesistente, viene ignorata
-];
+array_pop($alunni);
+array_shift($alunni);
 
-var_dump($user); // visualizza il contenuto dell'array
+unset($alunni[1]);
+$alunni = array_values($alunni);
 
-echo $user['nome'] . ' ' . $user['cognome'] . ' ha ' . $user['eta'] . ' anni'; // visualizza il nome, cognome e età dell'utente
-
-echo "{$user['nome']} {$user['cognome']} ha {$user['eta']} anni"; // visualizza lo stesso risultato utilizzando la sintassi di interpolazione delle stringhe
-
-// extract
-extract($user); // estrae le chiavi dell'array in variabili locali
-echo $nome . ' ' . $cognome . ' ha ' . $eta . ' anni'; // visualizza lo stesso risultato utilizzando le variabili locali
-
-echo $nome . ' ' . $cognome . ' ha ' . $eta . ' anni';
-
+array_splice($alunni, 2, 1, ['Luca', 'Marco']);
+var_dump($alunni);
