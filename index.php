c5288edeed479a3652f261da83882acebde5580c
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-23 16:19:43 +0200

    Update index.php to include examples of type casting in PHP

--- index.php
+++ index.php
@@ -1,45 +1,49 @@
 <?php
 
 /**
- * Questo esempio mostra come creare e utilizzare array multidimensionali in PHP.
+ * Type casting: conversione di un tipo di dato in un altro tipo di dato
  *
- * Un array multidimensionale è un array che contiene altri array.
- * Ogni sottomatrice può avere una dimensione diversa dall'altra.
- * Le matrici possono avere chiavi stringhe o numeriche.
- *
- * Nel seguente esempio, abbiamo due array multidimensionali:
- *
- * - `$multidimensionale` è un array di tre elementi, ciascuno dei quali è un array con due campi:
- *   `ISBN` e `Titolo`.
- *
- * - `$multidimensionale2` è un array associativo con tre sotto-matrici.
- *   Le sotto-matrici hanno chiavi stringhe: 'Primo', 'Secondo' e 'Terzo'.
- *   Ognuna di queste sotto-matrici è un array associativo con due campi: `ISBN` e `Titolo`.
+ * PHP offre diverse funzioni di type casting:
+ * - int: conversione in integer
+ * - bool: conversione in booleano
+ * - float: conversione in floating point number
+ * - string: conversione in stringa
+ * - array: conversione in array
+ * - object: conversione in oggetto
  */
 
-$multidimensionale = [
-  ['ISBN' => 1234, 'Titolo' => 'Libro 1'],
-  ['ISBM' => 1234, 'Titolo' => 'Libro 2'],
-  ['ISBM' => 1234, 'Titolo' => 'Libro 3'],
-];
+// Esempio di casting da stringa a intero
+$stringa = "22";
 
-var_dump($multidimensionale);
+$intero = (int)$stringa;
 
-// Possiamo accedere ad un elemento multidimensionale come facciamo con un array normale.
-$item = $multidimensionale[2];
-echo $item['Titolo'];
+var_dump($intero); // Output: int(22)
 
-// Oppure possiamo accedere direttamente ad un elemento multidimensionale.
-echo $multidimensionale[2]['Titolo'];
 
-// Esempio di array multidimensionale associativo
-$multidimensionale2 = [
-  'Primo' => ['ISBN' => 1234, 'Titolo' => 'Libro 1'],
-  'Secondo' =>['ISBM' => 1234, 'Titolo' => 'Libro 2'],
-  'Terzo' => ['ISBM' => 1234, 'Titolo' => 'Libro 3'],
-];
+// Esempio di casting da booleano a intero
+$booleano = true;
 
-var_dump($multidimensionale2);
+$intero_bool = (int) $booleano;
 
-// Possiamo accedere ad un elemento multidimensionale associativo come facciamo con un array normale.
-echo $multidimensionale2['Terzo']['Titolo'];
+var_dump($intero_bool); // Output: int(1)
+
+
+/* -------------------------------------------------------------------------- */
+
+// Esempio di casting da intero a array
+$numero = 30;
+
+$array = (array) $numero;
+
+var_dump($array); // Output: array(1) { [0]=> int(30) }
+
+/* -------------------------------------------------------------------------- */
+
+// Esempio di casting da array a oggetto
+$array = [1, 'Corso PHP', true];
+
+$oggetto = (object) $array;
+
+var_dump($oggetto); // Output: object(stdClass)#1 (3) { [0]=> int(1) [1]=> string(10) "Corso PHP" [2]=> bool(true) }
+
+var_dump($oggetto);
