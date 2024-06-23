e52c18a223018dc57dfcbdf285752f002fbe8f50
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-23 16:48:04 +0200

    Add docblock to `informazioniCorso` function in `index.php`

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Calcola il doppio di un valore.
+ *
+ * @param int $valore Il valore da doppiare.
+ * @return int Il doppio del valore dato in input.
+ */
 function informazioniCorso($valore){
 
   return $valore * 2;
