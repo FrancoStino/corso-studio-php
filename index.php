984a5d197ee8da9b77930f7e768d525338debe72
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-24 14:34:14 +0200

    Aggiorna index.php per mostrare l'uso delle sessioni invece dei cookie

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
 
 /**
- * In questo script vengono impostati dei cookie.
- *
- * Un cookie è un piccolo pezzo di dati che viene salvato sul
- * browser dell'utente e che viene invio nell'header della richiesta
- * HTTP quando l'utente accede ad una pagina web.
- *
- * In questo caso viene impostato un cookie chiamato "coloriPreferiti"
- * il cui valore è "Scuri". Il cookie verrà salvato sul browser dell'utente
- * per 30 secondi (il valore di $scadenzaCookie).
+ * In questo esempio viene mostrato come utilizzare i cookie per gestire le sessioni.
+ * 
+ * La funzione session_start() viene utilizzata per avviare una sessione.
+ * 
+ * Successivamente viene impostato il valore della variabile di sessione 'nome' su 'Mario'.
+ * 
+ * Infine viene stampato l'ID della sessione corrente utilizzando la funzione session_id().
  */
 
-// Calcola il timestamp di scadenza del cookie
-$scadenzaCookie = time() + 30;
+session_start();
 
-// Imposta il cookie
-setcookie("coloriPreferiti", "Scuri", $scadenzaCookie);
+$_SESSION['nome'] = 'Mario';
 
-// Visualizza i cookie salvati nel browser
-var_dump($_COOKIE);
+echo "ID della sessione corrente: " . session_id();
 
