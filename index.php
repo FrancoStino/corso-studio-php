44cbfadbc1fa5a947553da7ee1b6cc00c6e13536
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-24 10:39:33 +0200

    Add PHP script documentation and improve form labels in index.php

--- index.php
+++ index.php
@@ -1,33 +1,26 @@
 <?php
+/**
+ * Questo script PHP riceve i dati inviati da un form tramite metodo GET.
+ * I dati sono memorizzati nell'array superglobale $_GET.
+ * Per stampare i valori dell'array, è stata utilizzata la funzione var_dump().
+ */
 
-// // Array superglobale GET
-// echo "Corso {$_GET['corso']} di realizzato da {$_GET['nome']}".PHP_EOL;
-
-
-// // Con extract
-
-// extract($_GET);
-
-// echo "Corso $corso di realizzato da $nome" . PHP_EOL;
-
+// Stampa l'array $_GET
 var_dump($_GET);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
   <title>Document</title>
 </head>
 <body>
   <form action="index.php" method="get">
-
-    <label for="corso">Corso</label>
+    <label for="corso">Corso: </label>
     <input type="text" name="corso" id="corso">
-    <label for="nome">Nome</label>
+    <label for="nome">Nome: </label>
     <input type="text" name="nome" id="nome">
     <input type="submit" value="Invia">
-
   </form>
 </body>
 </html>
