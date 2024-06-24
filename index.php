59d978183eb58c43b090bb97fc0bf7bfdaa54a6f
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-24 10:47:57 +0200

    Refactor index.php to display $_SERVER superglobal instead of form and $_GET data

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
-/**
- * Questo script PHP riceve i dati inviati da un form tramite metodo GET.
- * I dati sono memorizzati nell'array superglobale $_GET.
- * Per stampare i valori dell'array, è stata utilizzata la funzione var_dump().
- */
 
-// Stampa l'array $_GET
-var_dump($_GET);
-?>
+// Array superglobale $_SERVER
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Document</title>
-</head>
-<body>
-  <form action="index.php" method="get">
-    <label for="corso">Corso: </label>
-    <input type="text" name="corso" id="corso">
-    <label for="nome">Nome: </label>
-    <input type="text" name="nome" id="nome">
-    <input type="submit" value="Invia">
-  </form>
-</body>
-</html>
+echo '<pre>';
+var_dump($_SERVER);
+echo '</pre>';
