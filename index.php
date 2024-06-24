63b1cee72ac28688bfa5a3cf47601bc9dc2b33a1
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-24 11:24:15 +0200

    Implement file upload functionality using $_FILES superglobal in index.php

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
 
-// Array superglobale $_SERVER
+// Array superglobale $_FILES
+// echo $_SERVER['DOCUMENT_ROOT'];
+if (isset($_POST['submit'])) {
+  $uploadPath = $_SERVER['DOCUMENT_ROOT'] . '/uploads/';
 
-echo '<pre>';
-var_dump($_SERVER);
-echo '</pre>';
+  $tmpFile = $_FILES['fileToUpload']['tmp_name'];
+
+
+  move_uploaded_file($tmpFile, $uploadPath . $_FILES['fileToUpload']['name']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+
+  <body>
+    <form action="" method="post" enctype="multipart/form-data">
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
+  </body>
+
+</html>
