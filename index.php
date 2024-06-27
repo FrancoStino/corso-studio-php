00a4f9108d55f35b2abb74f0539f41e195d46969
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-27 20:44:02 +0200

    Add example and explanation for ternary operator in PHP

--- index.php
+++ index.php
@@ -1,39 +1,32 @@
 <?php
 
-// Operatori su array
+/**
+ * Operatore ternario ?:
+ *
+ * Permette di scrivere in modo più conciso una condizione che restituisce un valore
+ * diverso a seconda del risultato della condizione.
+ *
+ * Sintassi:
+ *
+ *      $valore = ($condizione) ? $valoreSeVero : $valoreSeFalso;
+ *
+ * Esempio:
+ *
+ *      $anni = 14;
+ *      $costoBiglietto = ($anni < 12) ? 5 : 10;
+ *      echo "Costo biglietto: " . $costoBiglietto;
+ *
+ * Output:
+ *
+ *      Costo biglietto: 10
+ *
+ * In questo esempio, se $anni è minore di 12, il valore di $costoBiglietto sarà 5,
+ * altrimenti sarà 10.
+ */
 
-// === stesse chiavi e stessi valori (confronto stretta uguaglianza. L'ordine dell'array è importante)
-// Ad esempio, se ho un array con una sola chiave, la confronto con un array con la stessa chiave e lo stesso valore
-// il risultato è true, se invece ho un array con una sola chiave e un altro array con una chiave diversa, lo stesso valore
-// il risultato è false
-$a = [1];
-$b = ["1"];
+$anni = 14;
 
-var_dump($a === $b); // false
+$costoBiglietto = ($anni < 12) ? 5 : 10;
 
+echo "Costo biglietto: " . $costoBiglietto;
 
-$c = ["valore1" => 1];
-$d = ["valore2" => "1"];
-
-var_dump($c === $d); // false
-
-
-// == stesse chiavi e stessi valori (confronto semplice uguaglianza. L'ordine dell'array non importa)
-// Ad esempio, se ho un array con una sola chiave, la confronto con un array con la stessa chiave e lo stesso valore
-// il risultato è true, se invece ho un array con una sola chiave e un altro array con una chiave diversa, lo stesso valore
-// il risultato è true. L'ordine degli elementi non conta
-
-$e = ["valore2" => 3, "valore1" => 1];
-$d = ["valore1" => 1, "valore2" => 2];
-
-var_dump($e == $d); // true
-
-
-// + Unione di array
-// Se ho due array, unione semplice (se le chiavi sono diverse) o sovrascritura (se le chiavi sono uguali)
-
-$f = ["pizzaPreferita" => "Margherita", 28 ];
-$g = ["pizzaPreferita" => "Capricciosa", 30, 'php' ];
-$h = $f + $g;
-
-var_dump($h); // ["pizzaPreferita" => "Capricciosa", 28, 30, 'php']
