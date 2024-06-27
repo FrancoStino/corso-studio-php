577d312c337e8e79bb78be785bb516319eb0b4fa
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-27 20:38:29 +0200

    Update index.php to demonstrate array operators and comparisons

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <?php
 
-/**
- * Esempio di precedenza tra operatori e short-circuit evaluation.
- *
- * L'operatore '&&' viene valutato da sinistra a destra. Se l'operando
- * a sinistra è falso, viene restituito subito falso, senza valutare l'operando
- * a destra. Altrimenti, viene valutato l'operando a destra.
- */
+// Operatori su array
 
-$testTrue = true;
-$testFalse = false;
+// === stesse chiavi e stessi valori (confronto stretta uguaglianza. L'ordine dell'array è importante)
+// Ad esempio, se ho un array con una sola chiave, la confronto con un array con la stessa chiave e lo stesso valore
+// il risultato è true, se invece ho un array con una sola chiave e un altro array con una chiave diversa, lo stesso valore
+// il risultato è false
+$a = [1];
+$b = ["1"];
 
-var_dump($testTrue and $testFalse); // bool(false)
+var_dump($a === $b); // false
 
-/**
- * Esempio di funzione che viene solamente eseguita se l'operando
- * a sinistra è vero.
- */
-function fn1()
-{
-  echo "Funzione " . __FUNCTION__ . " eseguita";
-}
 
-false && fn1(); // funzione non eseguita
-true && fn1(); // funzione eseguita
+$c = ["valore1" => 1];
+$d = ["valore2" => "1"];
+
+var_dump($c === $d); // false
+
+
+// == stesse chiavi e stessi valori (confronto semplice uguaglianza. L'ordine dell'array non importa)
+// Ad esempio, se ho un array con una sola chiave, la confronto con un array con la stessa chiave e lo stesso valore
+// il risultato è true, se invece ho un array con una sola chiave e un altro array con una chiave diversa, lo stesso valore
+// il risultato è true. L'ordine degli elementi non conta
+
+$e = ["valore2" => 3, "valore1" => 1];
+$d = ["valore1" => 1, "valore2" => 2];
+
+var_dump($e == $d); // true
+
+
+// + Unione di array
+// Se ho due array, unione semplice (se le chiavi sono diverse) o sovrascritura (se le chiavi sono uguali)
+
+$f = ["pizzaPreferita" => "Margherita", 28 ];
+$g = ["pizzaPreferita" => "Capricciosa", 30, 'php' ];
+$h = $f + $g;
+
+var_dump($h); // ["pizzaPreferita" => "Capricciosa", 28, 30, 'php']
