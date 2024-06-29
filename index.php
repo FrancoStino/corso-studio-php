12989894118cca6c595835ad80f0785dc21b2d22
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-29 18:15:45 +0200

    Refactor index.php to use if-else statements for navigation and age check

--- index.php
+++ index.php
@@ -1,24 +1,49 @@
 <?php
 
-/**
- * Esempio di utilizzo dell'operatore null coalescing (??).
- *
- * L'operatore null coalescing (??), introdotto in PHP 7, permette di fornire un valore
- * di default in caso di un valore null o inesistente.
- *
- * La sintassi è la seguente:
- *
- * $var = $valore ?? $valoreDefault;
- *
- * Ad esempio, nel seguente codice viene letta la variabile $_GET['page'] e, se esiste e
- * non è null, viene assegnata a $page. In caso contrario, viene assegnato il valore
- * di default 1.
- *
- * @link http://php.net/manual/en/migration70.new-features.php#migration70.new-features.null-coalesce-op
- */
+// if else
 
-// https://localhost/corsi/php/index.php?page=2
 
-$page = $_GET['page'] ?? 1;
+$isLoggedd = false;
 
-echo $page;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang = "en">
+
+  <head>
+    <meta charset = "UTF-8">
+    <meta name    = "viewport" content = "width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+
+  <body>
+    <nav>
+      <ul>
+        <li><a href = "">Home</a></li>
+        <li><a href = "">Suka</a></li>
+        <?php
+        if ($isLogged === true): ?>
+          <li><a href = "">Ciaooooooo</a></li>
+
+        <?php else: ?>
+          <li><a href = "">Addio</a></li>
+        <?php endif; ?>
+      </ul>
+    </nav>
+  </body>
+
+</html>
+
+<?php
+
+$anni = 18;
+
+if ($anni <= 12) {
+  echo "Ha meno di 12 anni";
+} elseif ($anni >= 12 && $anni < 18) {
+  echo "Ha tra i 12 e i 17 anni";
+} else {
+  echo "Ha 18 anni o più";
+}
