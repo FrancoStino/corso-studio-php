1a2d902c20b4d38323a473eed6897b6abed906df
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-30 19:54:21 +0200

    Implement secure random password generation in index.php

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <?php
 
-/**
- * In questo esempio vengono illustrate le differenze tra require e include.
- *
- * require e include sono entrambe funzioni PHP che permettono di includere
- * un file all'interno di un altro. La differenza principale tra le due
- * consiste nel fatto che require se il file non viene trovato genera un
- * errore e interrompe l'esecuzione del programma, mentre include solo genera
- * un avviso e continua l'esecuzione.
- *
- * In questo caso, se il file 'funzioni.php' non viene trovato, l'esecuzione
- * del programma viene interrotta con un errore. Se il file fosse incluso
- * invece mediante include, l'avviso generato sarebbe visibile nel log degli
- * errori ma l'esecuzione del programma continuerebbe.
- */
+// Generazione password sicura di 16 caratteri
 
-// require e include
+$caratteri = [
+  'abcdefghilmnopqrstuvz',
+  'ABCDEFGHILMNOPQRSTUVZ',
+  '1234567890',
+  '!@#$%^&*()_+',
+];
 
-require 'funzioni.php';
+$password = '';
 
+for ($i = 0; $i < 16; $i++) {
+  $selezione = rand(0, 3);
+  $last = strlen($caratteri[$selezione]) - 1;
+  $password .= $caratteri[$selezione][rand(0, $last)];
+  $suka = true;
+}
 
-// Chiama la funzione test() definita in 'funzioni.php'
-test();
+echo $password;
 
-// Chiama la funzione test2() definita in 'funzioni.php'
-test2();
-
-
-
-echo "Codice eseguito";
