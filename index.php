9e6dfa612610d76612a3659c95eef00867553eea
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-30 17:06:00 +0200

    Refactor `index.php` to use PHP 8 `match` expression for error handling; update `.lando.yml` to use PHP 8.3 and nginx.

--- index.php
+++ index.php
@@ -1,24 +1,18 @@
 <?php
 
-/**
- * This PHP script demonstrates the usage of the 'switch' statement.
- * It takes an error code as input and prints an appropriate error message.
- *
- * @param int $errorCode The error code to be processed.
- *
- * @return void
- */
+// Questo è un esempio di utilizzo della funzione 'match' introdotta in PHP 8.
+// La funzione 'match' permette di eseguire una serie di istruzioni in base a un valore specifico.
+// In questo caso, viene verificato il valore di $errorCode e viene restituito un messaggio corrispondente.
 
-$errorCode = 404;
+$errorCode = 502; // Imposta il valore di $errorCode a 502
 
-switch ($errorCode) {
-  case 404: 
-    echo 'Page not found';
-    break;
-  case 500: 
-    echo 'Internal server error';
-    break;
-  default: 
-    echo 'Unknown error';
-}
+// Utilizzo della funzione 'match' per verificare il valore di $errorCode
+// e restituire un messaggio corrispondente.
+$msg = match($errorCode) {
+  404 => 'Not Found', // Se $errorCode è uguale a 404, restituisce 'Not Found'
+  500 => 'Internal Server Error', // Se $errorCode è uguale a 500, restituisce 'Internal Server Error'
+  502 => 'Bad Gateway', // Se $errorCode è uguale a 502, restituisce 'Bad Gateway'
+  default => 'Unknown Error', // Se $errorCode non corrisponde ad alcuna delle opzioni precedenti, restituisce 'Unknown Error'
+};
 
+echo $msg; // Stampa il messaggio corrispondente al valore di $errorCode
