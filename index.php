ddcaee4294653cb7af02febcfee6b214a4fe1433
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-30 12:16:35 +0200

    Add email field to user array and improve code formatting in index.php

--- index.php
+++ index.php
@@ -4,23 +4,25 @@
  * In questo script vengono stampati tutti i valori presenti nell'array $user,
  * utilizzando un ciclo foreach.
  * 
- * Per ogni elemento dell'array $user, viene stampato il relativo chiave seguito
+ * Per ogni elemento dell'array $user							, viene stampato il relativo chiave seguito
  * dal valore. Se il valore dell'elemento è un array, viene stampato come
  * stringa separata da virgole.
  */
 
 $user = [
-  'name' => 'John',
+  'name'       => 'John',
   'profession' => ['developer', 'designer'],
-  'age' => 30
+  'age'        => 30,
+  'email'      => 'john@example.com',
 ];
 
-foreach($user as $key => $valore){
+foreach ($user as $key => $valore) {
   echo "$key: ";
-  if(!is_array($valore)){
+  if (!is_array($valore)) {
     echo "$valore" . PHP_EOL;
   } else {
     echo implode(',', $valore) . PHP_EOL;
   }
 }
+
 
