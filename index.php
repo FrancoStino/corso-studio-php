5d75b126946a656c0b5a8550263ebdc96d179b76
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-30 12:56:02 +0200

    Refactor index.php to implement dice roll exercise with for and while loops, including break and continue logic

--- index.php
+++ index.php
@@ -1,28 +1,40 @@
 <?php
 
 /**
- * In questo script vengono stampati tutti i valori presenti nell'array $user,
- * utilizzando un ciclo foreach.
- * 
- * Per ogni elemento dell'array $user							, viene stampato il relativo chiave seguito
- * dal valore. Se il valore dell'elemento è un array, viene stampato come
- * stringa separata da virgole.
+ * Esercizio di lancio di due dadi e verifica del risultato.
+ * Utilizza un ciclo for per lanciare i dadi e un ciclo while 
+ * per convalidare il risultato.
  */
+function lanciaDadi(){
+  // Array contenente i valori dei dadi
+  $dado1 = [1, 2, 3, 4, 5, 6];
+  $dado2 = [1, 2, 3, 4, 5, 6];
+  // Ritorna la somma dei valori dei due dadi
+  return $dado1[rand(0, 5)] + $dado2[rand(0, 5)];
+}
 
-$user = [
-  'name'       => 'John',
-  'profession' => ['developer', 'designer'],
-  'age'        => 30,
-  'email'      => 'john@example.com',
-];
-
-foreach ($user as $key => $valore) {
-  echo "$key: ";
-  if (!is_array($valore)) {
-    echo "$valore" . PHP_EOL;
-  } else {
-    echo implode(',', $valore) . PHP_EOL;
+// Esegue il ciclo for per lanciare i dadi
+$lanci = 20;
+for ($i = 1; $i <= $lanci; $i++) {
+  $risultato = lanciaDadi();
+  echo "Lancio numero: $i, risultato: $risultato <br>";
+  // Se il risultato è uguale a 7, allora si vince
+  if ($risultato === 7) {
+    echo "Hai vinto, il numero fortunato è $risultato";
+    break; // Esce dal ciclo for
   }
 }
 
+echo "<br><br>/* -------------------------------------------------------------------------- */<br><br>";
+
+// Esegue il ciclo for per lanciare i dadi
+// Se il risultato è uguale a 7, allora si continua con il prossimo lancio
+for ($i = 1; $i <= $lanci; $i++) {
+  $risultato = lanciaDadi();
+  echo "Lancio numero: $i,risultato: $risultato <br>";
+  if ($risultato === 7) {
+    echo "Hai vinto, il numero fortunato è $risultato <br>";
+    continue; // Viene eseguito il prossimo ciclo
+  }
+}
 
