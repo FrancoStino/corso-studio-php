2a3917946b0c1ae8bfbfa7bb6b9c88c6f52e8116
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-06-30 16:45:05 +0200

    Refactor index.php to demonstrate 'switch' statement for error code handling

--- index.php
+++ index.php
@@ -1,40 +1,24 @@
 <?php
 
 /**
- * Esercizio di lancio di due dadi e verifica del risultato.
- * Utilizza un ciclo for per lanciare i dadi e un ciclo while 
- * per convalidare il risultato.
+ * This PHP script demonstrates the usage of the 'switch' statement.
+ * It takes an error code as input and prints an appropriate error message.
+ *
+ * @param int $errorCode The error code to be processed.
+ *
+ * @return void
  */
-function lanciaDadi(){
-  // Array contenente i valori dei dadi
-  $dado1 = [1, 2, 3, 4, 5, 6];
-  $dado2 = [1, 2, 3, 4, 5, 6];
-  // Ritorna la somma dei valori dei due dadi
-  return $dado1[rand(0, 5)] + $dado2[rand(0, 5)];
-}
 
-// Esegue il ciclo for per lanciare i dadi
-$lanci = 20;
-for ($i = 1; $i <= $lanci; $i++) {
-  $risultato = lanciaDadi();
-  echo "Lancio numero: $i, risultato: $risultato <br>";
-  // Se il risultato è uguale a 7, allora si vince
-  if ($risultato === 7) {
-    echo "Hai vinto, il numero fortunato è $risultato";
-    break; // Esce dal ciclo for
-  }
-}
+$errorCode = 404;
 
-echo "<br><br>/* -------------------------------------------------------------------------- */<br><br>";
-
-// Esegue il ciclo for per lanciare i dadi
-// Se il risultato è uguale a 7, allora si continua con il prossimo lancio
-for ($i = 1; $i <= $lanci; $i++) {
-  $risultato = lanciaDadi();
-  echo "Lancio numero: $i,risultato: $risultato <br>";
-  if ($risultato === 7) {
-    echo "Hai vinto, il numero fortunato è $risultato <br>";
-    continue; // Viene eseguito il prossimo ciclo
-  }
+switch ($errorCode) {
+  case 404: 
+    echo 'Page not found';
+    break;
+  case 500: 
+    echo 'Internal server error';
+    break;
+  default: 
+    echo 'Unknown error';
 }
 
