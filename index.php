4aa6a9289e1e161ae84d47a79e5a1b19fd76f1df
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-03 20:51:42 +0200

    refactor: update index.php to improve code readability and structure

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
-// Calcolare il valore minimo, maggiore e medio di un array
+/**
+ * Calcola il valore minimo, maggiore e medio di un array di voti.
+ *
+ * @param array $votiEsami Array di voti.
+ * @return void
+ */
+function calcolaValoriMinimoMaggioreEMedia(array $votiEsami): void
+{
+    $min = min($votiEsami);
+    $max = max($votiEsami);
+    $avg = array_sum($votiEsami) / count($votiEsami);
 
+    echo "Minimo: $min, Massimo: $max, Media: $avg";
+}
+
+// Chiama la funzione con l'array di voti
 $votiEsami = [28, 30, 29, 26, 18];
-
-$min = min($votiEsami);
-$max = max($votiEsami);
-$avg = array_sum($votiEsami) / count($votiEsami);
-
-echo "min: $min, max: $max, avg: $avg";
+calcolaValoriMinimoMaggioreEMedia($votiEsami);
