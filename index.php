5bd5f18cdbb7c8e5a3876d3ecab31c0960629b82
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-04 18:16:23 +0200

    refactor: update validation methods in index.php

--- index.php
+++ index.php
@@ -1,24 +1,18 @@
 <?php
 
-// FUNZIONE filter_var:
-// sanifica delle variabili con filter_var
+// Validare le seguenti variabili con filter_var
 
-// Esempio di utilizzo: sanificare un numero di telefono
-$tel = "+39 abc 333 444 555";
-$telClean = filter_var($tel, FILTER_SANITIZE_NUMBER_INT);
-echo "<p>Il numero di telefono originale è: <strong>$tel</strong><br>";
-echo "Il numero di telefono sanificato è: <strong>$telClean</strong></p>";
+// Validazione numero
+$numero = "15";
+$test = filter_var($numero, FILTER_VALIDATE_INT, [
+  "options" => ["min_range" => 1, "max_range" => 10]
+]);
+var_dump($test);
 
+// Validazione email
 
-// Esempio di utilizzo: sanificare una stringa da tag a apici
-$str = "<script>alert('ciao')</script>";
-$strClean = filter_var($str, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-echo "<p>La stringa originale è: <strong>$str</strong><br>";
-echo "La stringa sanificata è: <strong>" . htmlspecialchars($strClean) . "</strong></p>";
+$email = "suka@gmail@gmail.com";
+$email = "suka@gmail.com";
 
-
-// Esempio di utilizzo: sanificare aggiungendo uno slash prima degli apici
-$query = "Insert into t1 (c1) values ('ciao');";
-echo "<p>La query originale è: <strong>$query</strong><br>";
-echo "La query sanificata è: <strong>" . addslashes($query) . "</strong><br>";
-echo "La query sanificata è: <strong>" . filter_var($query, FILTER_SANITIZE_ADD_SLASHES) . "</strong></p>";
+$test = filter_var($email, FILTER_VALIDATE_EMAIL);
+var_dump($test);
