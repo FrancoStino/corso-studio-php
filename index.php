7c11f397dab1131a7022c2c09bd120dba6ec73a2
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-04 20:59:48 +0200

    refactor: update comments and validation in index.php

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 
-// Validare le seguenti variabili con filter_var
+/**
+ * Validazione delle variabili con `filter_var()`
+ *
+ * Questo script mostra come utilizzare la funzione `filter_var()` per
+ * validare le variabili.
+ *
+ * La funzione `filter_var()` prende in input una variabile e la esegue
+ * attraverso un filtro specificato. In questo caso viene utilizzato il
+ * filtro `FILTER_VALIDATE_INT` per validare che una variabile sia un numero
+ * intero.
+ *
+ * La funzione `filter_var()` restituisce il valore validato se la validazione
+ * è andata a buon fine, altrimenti restituisce `false`.
+ *
+ * Per la validazione di un numero intero si può anche specificare dei
+ * parametri di validazione utilizzando l'array `options`. In questo caso
+ * viene utilizzato per specificare che il numero deve essere compreso tra 1
+ * e 10.
+ *
+ * La funzione `filter_var()` può essere utilizzata anche per validare un
+ * indirizzo email con il filtro `FILTER_VALIDATE_EMAIL`.
+ */
 
 // Validazione numero
 $numero = "15";
