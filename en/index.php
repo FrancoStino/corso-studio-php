96d711916f397b188b9d245ef62a96677b9ccfa9
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-05 17:25:08 +0200

    refactor: remove unused files and functions, add lib.php

--- en/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "Sei nella versione inglese";
--- index.php
+++ index.php
@@ -1,43 +1,5 @@
 <?php
 
-// Type hinting - nullable e union types
+require 'lib.php';
 
-declare(strict_types=1);
-
-
-function fn1( int|float $p1 )
-{
-
-}
-
-function fn2( string|int|bool $p1 )
-{
-
-}
-
-fn1( 1 );
-fn1( 3.1415 );
-fn2( true );
-fn2( 123 );
-fn2( "ciao" );
-
-
-// Type hinting - array type
-
-function fn3( array $p1 )
-{
-
-}
-
-fn3( [] );
-
-
-// Type hinting - null float
-
-function fn4( ?float $p1 )
-{
-
-}
-
-fn4( null );
-fn4( 3.1415 );
+mostraSequenza( 1, 10, 5 );
