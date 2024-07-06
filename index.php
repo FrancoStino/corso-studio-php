56733a29fbf97a7c4aeb012068ef59f1bfa70f62
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-06 13:09:40 +0200

    refactor: update index.php with new functions and data structures

--- index.php
+++ index.php
@@ -1,41 +1,83 @@
 <?php
 
-// Arrow function (PHP 7.4)
+/**
+ * Funzione per aumentare i voti minori di 30 di un array.
+ *
+ * @param int $voto Il voto da processare.
+ * @return int Il voto aumentato di 1 se minore di 30, altrimenti il voto originale.
+ */
+function aumentaVoto( int $voto ): int
+{
+  return $voto < 30 ? $voto + 1 : $voto;
+}
 
-// Ordina l'array $a in ordine decrescente
-$a = array( 3, 2, 5, 6, 1, 3 );
+// Array map
 
-usort( $a, function ($a1, $a2) {
-  return $a2 <=> $a1;
-} );
+$voti = [ 
+  24,
+  30,
+  29,
+  10,
+];
 
-// Stampa l'array ordinato
-print_r( $a );
+/**
+ * Array contenente i voti aumentati di 1 se minori di 30.
+ * @var array
+ */
+$votiAlterati = array_map( 'aumentaVoto', $voti );
 
-// Arrow function (non è necessario usare il return)
-
-// Ordina l'array $a in ordine decrescente
-$a = array( 1, 2, 3, 4, 5, 6 );
-
-usort( $a, fn( $a1, $a2 ) => $a2 <=> $a1 );
-
-print_r( $a );
+// var_dump( $votiAlterati );
 
 /* -------------------------------------------------------------------------- */
 
-// use: permette di accedere alle variabili esterne all'interno di una funzione
-$b = 20;
+/**
+ * Array contenente i prodotti con i prezzi scontati.
+ * @var array
+ */
+$prodotti = [ 
+  [ 
+    'nome'   => 'Mouse Gaming',
+    'prezzo' => 50,
+  ],
+  [ 
+    'nome'   => 'Dell 27 pollici',
+    'prezzo' => 380,
+  ],
+  [ 
+    'nome'   => 'Dell 32 Pollici',
+    'prezzo' => 450,
+  ],
+  [ 
+    'nome'   => 'Iphone 15',
+    'prezzo' => 899,
+  ],
+  [ 
+    'nome'   => 'Iphone 15 Max0',
+    'prezzo' => 1299,
+  ],
+];
 
-// Creazione di una funzione anonima che somma il parametro $a alla variabile $b
-$anonima1 = function ($a) use ($b) {
-  return $a + $b;
-};
+/**
+ * Funzione per applicare lo sconto del 5% al prezzo di un prodotto.
+ *
+ * @param array $prodotto Array con i dati del prodotto.
+ * @return array Array con i dati del prodotto con il prezzo scontato.
+ */
+function applicaSconto( array $prodotto ): array
+{
+  $sconto         = $prodotto[ 'prezzo' ] * 5 / 100;
+  $prezzoScontato = $prodotto[ 'prezzo' ] - $sconto;
+  $prezzoScontato = number_format( $prezzoScontato, 2, ',', '.' );
+  // Utilizzo la sintassi di array spread per creare un nuovo array che è una copia dell'array originale,
+  // ma con l'aggiunta del nuovo campo 'prezzo' con il valore calcolato.
+  return [ ...$prodotto, 'prezzo' => $prezzoScontato ];
+}
 
-// Chiamata della funzione anonima
-echo $anonima1( 10 ) . "\n";
+/**
+ * Array contenente i prodotti con i prezzi scontati.
+ * @var array
+ */
+$prodottiScontati = array_map( 'applicaSconto', $prodotti );
 
-// Creazione di una funzione anonima che somma il parametro $a alla variabile $b
-$anonima2 = fn( $a ) => $a + $b;
-
-// Chiamata della funzione anonima
-echo $anonima2( 10 );
+// dump dei prodotti scontati
+var_dump( $prodottiScontati );
