5837ac702afb8afb5bd3589f31e7a503643ba14d
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-08 16:25:42 +0200

    feat: create new tmp.php file with Tmp class

---
+++ tmp.php
@@ -0,0 +1,19 @@
+<?php
+
+class Tmp{
+//   private $p1;
+//     public function getP1(){
+//       return $this->p1;
+//     }
+//     public function setP1(int $valore){
+//       $this->p1 = $valore;
+//     }
+
+public int $p1;
+
+
+}
+
+$tmp = new Tmp;
+$tmp->p1 = 10;
+echo $tmp->p1;
