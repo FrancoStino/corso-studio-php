4723e4d30451d2f51586302b039d6a2759674a47
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-08 10:38:12 +0200

    refactor: update index.php and lib/Corso.php to use strict types and improve code readability

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
  * Creazione di due oggetti della classe Corso.
  * Stampa dei dati dei due oggetti.
  */
-
+declare (strict_types = 1);
 require_once 'lib/Corso.php';
 
 // Creazione di due oggetti della classe Corso.
-$corsoPHP            = new Corso("Corso PHP", "Mario Rossi", "Backend", 2000);
-echo $corsoPHP->riepilogo() ."<br>";
+$corsoPHP = new Corso(
+  "Corso PHP",
+  "Mario Rossi",
+  "Backend",
+  2000
+);
+echo $corsoPHP->riepilogo() . "<br>";
 
-$corsoJava           = new Corso("Corso Java", "Maria Bianchi", "Frontend", 7500);
+$corsoJava = new Corso( "Corso Java", "Maria Bianchi", "Frontend", 7500 );
 echo $corsoJava->riepilogo();
+
--- lib/Corso.php
+++ lib/Corso.php
@@ -1,34 +1,22 @@
 <?php
 
-class Corso
-{
-  /**
-   * @var string $titolo Títolo del corso.
-   * @var string $autore Autore del corso.
-   * @var string $categoria Categoria del corso.
-   * @var int $prezzo Prezzo del corso.
-   */
-  public $titolo    = 'Corso X';
-  public $autore    = 'Mario Rossi';
-  public $categoria = 'Senza categoria';
-  public $prezzo    = 0;
+class Corso {
 
-  public function __construct ( $titoloInInput, $autoreInInput, $categoriaInInput, $prezzoInInput )
-  {
-    $this->titolo    = $titoloInInput;
-    $this->autore    = $autoreInInput;
-    $this->categoria = $categoriaInInput;
-    $this->prezzo    = $prezzoInInput;
-  }
+  public function __construct (
+    public string $titolo,
+    public string $autore,
+    public string $categoria,
+    public int|float $prezzo = 0
+  ) {}
 
   /**
    * Riepilogo del corso.
    *
    * @return string Riepilogo del corso.
    */
-  public function riepilogo ()
-  {
-    return "Titolo: " . $this->titolo . '<br>' .
+  public function riepilogo () {
+    return
+      "Titolo: " . $this->titolo . '<br>' .
       "Autore: " . $this->autore . '<br>' .
       "Categoria: " . $this->categoria . '<br>' .
       "Prezzo: " . $this->prezzo . '<br>';
