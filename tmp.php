0d0a8457c72d7d0f1a658097d18dd7c3608713e6
Davide Ladisa <davide.ladisa@sideagroup.com>
2024-07-13 17:13:33 +0200

    __ refactor: update class A and B with private constructor and static getInstance method__

--- tmp.php
+++ tmp.php
@@ -1,23 +1,49 @@
 <?php
 
+/**
+ * Classe A
+ *
+ * Classe che definisce un costruttore privato e un metodo di istanza statico per
+ * la creazione di un'istanza della classe stessa o di una sua sottoclasse.
+ */
 class A
 {
-  public $p1 = "p1";
+  /**
+   * Costruttore privato
+   *
+   * Costruttore che viene invocato quando viene creato un'istanza della classe.
+   * Poiché è privato, non può essere chiamato direttamente dall'esterno della classe.
+   */
+  private function __construct () {
+    echo "Costruttore di A invocato<br>";
+  }
 
-  public function fna () {
-
-    echo __METHOD__ . "<br>";
+  /**
+   * Metodo di istanza statico getInstance
+   *
+   * Metodo statico che restituisce un'istanza della classe stessa o di una sua sottoclasse.
+   * Utilizza la parola chiave "static" per accedere al costruttore della classe stessa.
+   *
+   * @return static L'istanza della classe stessa o di una sua sottoclasse.
+   */
+  public static function getInstance () {
+    return new static();
   }
 }
 
-class B extends A
-{
+// Creazione di un'istanza della classe A
+$a = A::getInstance();
 
-  public function fnb () {
-    echo parent::$p1;
-    parent::fna();
-  }
-}
-// Esegui fna
+/**
+ * Classe B
+ *
+ * Sottoclasse di A che non definisce nessun metodo o proprietà aggiuntiva.
+ */
+class B extends A { }
 
-(new B)->fnb();
+// Creazione di un'istanza della sottoclasse B
+$istance = B::getInstance();
+
+// Stampa a video l'istanza della sottoclasse B utilizzando la funzione var_dump
+var_dump( $istance );
+
