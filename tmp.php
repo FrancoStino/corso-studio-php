04c5268ca92401d9f0676222f32273aec30d1275
Davide <davide.ladisa@sideagroup.com>
2024-08-18 20:45:23 +0200

    refactor: Improve FileManager with private method and exceptions

--- tmp.php
+++ tmp.php
@@ -4,49 +4,61 @@
  * Classe FileManager
  * 
  * Gestisce la lettura e la scrittura di file
+ * 
+ * La classe FileManager ha un costruttore che accetta il nome del file da gestire
+ * Il costruttore esegue il metodo checkFileExists() che verifica se il file esiste
+ * Se il file non esiste, lancia un'eccezione
+ * 
+ * Il metodo checkFileExists() restituisce un booleano che indica se il file esiste o no
  */
 class FileManager
 {
+
     /**
      * Costruttore della classe FileManager
      * 
      * @param string $fileName Il nome del file da gestire
      */
-    public function __construct(string $fileName) {
+    public function __construct(private string $fileName) {
+
+        /**
+         * Verifica se il file esiste
+         * 
+         * Se il file non esiste, lancia un'eccezione
+         */
+        try {
+            $this->checkFileExists();
+        } catch (Exception $e) {
+            var_dump($e->getTrace());
+            echo "Messaggio:" . $e->getMessage();
+        }
+    }
+
+    /**
+     * Verifica se il file esiste
+     * 
+     * @return bool true se il file esiste, false altrimenti
+     * @throws Exception Se il file non esiste
+     */
+    private function checkFileExists(): bool {
+
         /**
          * Verifica se il file esiste
          */
-        try {
-            if(!file_exists($fileName)) {
-                /**
-                 * Se il file non esiste, lancia un'eccezione
-                 */
-                throw new Exception(
-                    message: "File not found",
-                    code: 404
-                );
-            }
-        } catch (Exception $e) {
+        if(!file_exists($this->fileName)) {
             /**
-             * Se l'eccezione viene catturata, stampa il messaggio di errore
+             * Se il file non esiste, lancia un'eccezione
              */
-            echo "Eccezione catturata: " . $e->getMessage() . "<br>";
-            echo "Codice errore: " . $e->getCode() . "<br>";
-            echo "File: " . $e->getFile() . "<br>";
-            echo "Riga errore: " . $e->getLine() . "<br>";
-            echo "<pre>";
-            /**
-             * Stampa anche il contenuto dell'eccezione
-             */
-            var_dump($e);
-            echo "</pre>";
+            throw new Exception(message: "File non trovato");
         }
 
         /**
-         * Stampa un messaggio per verificare se il costruttore viene eseguito
+         * Se il file esiste, restituisce true
          */
-        echo 2;
+        return true;
+
     }
+
 }
 
 /**
@@ -54,9 +66,4 @@ class FileManager
  * 
  * Viene creato un oggetto FileManager con il nome del file "error_log.txt"
  */
-$errorLogManager = new FileManager("error_log.txt");
-
-/**
- * Stampa un messaggio per verificare se il codice viene eseguito
- */
-echo 1;
+$errorLogManager = new FileManager("error.log");
